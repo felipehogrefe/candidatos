9c402c0e7312fa4d3d917eef04e1458a25d98d57
felipehogrefe <felipehogrefe@gmail.com>
2020-11-09 17:12:12 -0300

    Altera tipo da pk do modelo candidato para big integer.

--- receitas/models.py
+++ receitas/models.py
@@ -38,7 +38,7 @@ class Candidato(models.Model):
     cargo = models.IntegerField(choices=CARGO_CHOICES)
     numero = models.IntegerField()
     partido = models.CharField(max_length=8)
-    id = models.IntegerField(primary_key=True)
+    id = models.BigIntegerField(primary_key=True)
 
     def __str__(self):
         return "{} - {}".format(self.nome, str(self.numero))
